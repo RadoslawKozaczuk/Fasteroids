470604c054c9fbe8b5fc8ddf44be88eaa765b3c1
Radoslaw <ko.rad@o2.pl>
2019-06-18 22:08:04 +0200

    Added skyboxes and basic skybox rotator

---
+++ Assets/Script/SkyboxRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+class SkyboxRotator : MonoBehaviour
+{
+    // should be dynamic based on player's movement
+    public float RotationSpeed = 0.5f;
+    float _currentRotation = 0f;
+
+    // later on also add the ability to change the sky box with game restart/level change/etc
+    // If you change the skybox in playmode, you have to use the DynamicGI.UpdateEnvironment function call to update the ambient probe.
+
+    void Update()
+    {
+        _currentRotation += RotationSpeed * Time.deltaTime;
+        RenderSettings.skybox.SetFloat("_Rotation", _currentRotation);
+    }
+}
